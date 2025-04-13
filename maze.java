bedc96198557ebff5c915bc565c21f8152b275ba
Azemaab <azemaabdybekova@gmail.com>
2025-04-13 23:09:15 +0600

    Add MazeGenerator class to create maze using recursive backtracking

--- maze.java
+++ maze.java
@@ -1,1 +1,11 @@
 import java.util.*;
+// Main class
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the width of the maze:");
+        int width = scanner.nextInt();
+        System.out.println("Enter the height of the maze:");
+        int height = scanner.nextInt();
+
+        MazeGenerator generator = new MazeGenerator(width, height);
