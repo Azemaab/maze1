5e2f49b3ed077f349ba26a606d1657c451307d90
Azemaab <azemaabdybekova@gmail.com>
2025-04-13 23:18:37 +0600

    Implement DFS with wall checking to find exit path

--- maze.java
+++ maze.java
@@ -25,4 +25,66 @@ class MazeGenerator {
         this.width = width;
         this.height = height;
         this.maze = new int[height][width][4];
+        this.visited = new boolean[height][width];
+        generateMaze(0, 0);
+    }
+
+    private void generateMaze(int x, int y) {
+        visited[y][x] = true;
+        Integer[] directions = {0, 1, 2, 3};
+        Collections.shuffle(Arrays.asList(directions));
+
+        for (int direction : directions) {
+            int nx = x, ny = y;
+
+            switch (direction) {
+                case 0: nx = x - 1; break;
+                case 1: ny = y - 1; break;
+                case 2: nx = x + 1; break;
+                case 3: ny = y + 1; break;
+            }
+
+            if (nx >= 0 && ny >= 0 && nx < width && ny < height && !visited[ny][nx]) {
+                maze[y][x][direction] = 1;
+                maze[ny][nx][(direction + 2) % 4] = 1;
+                generateMaze(nx, ny);
+            }
+        }
+    }
+}
+
+// Pathfinder class
+class Pathfinder {
+    public static boolean[][] findPath(int[][][] maze) {
+        int height = maze.length;
+        int width = maze[0].length;
+        boolean[][] visited = new boolean[height][width];
+        boolean[][] path = new boolean[height][width];
+
+        dfs(maze, 0, 0, visited, path);
+        return path;
+    }
+
+    private static boolean dfs(int[][][] maze, int x, int y, boolean[][] visited, boolean[][] path) {
+        if (x == maze[0].length - 1 && y == maze.length - 1) {
+            path[y][x] = true;
+            return true;
+        }
+
+        visited[y][x] = true;
+        int[][] directions = {{-1, 0, 0}, {0, -1, 1}, {1, 0, 2}, {0, 1, 3}};
+
+        for (int[] dir : directions) {
+            int nx = x + dir[0], ny = y + dir[1], wall = dir[2];
+
+            if (nx >= 0 && ny >= 0 && nx < maze[0].length && ny < maze.length &&
+                maze[y][x][wall] == 1 && !visited[ny][nx]) {
+
+                if (dfs(maze, nx, ny, visited, path)) {
+                    path[y][x] = true;
+                    return true;
+                }
+            }
+        }
+        return false;
         
