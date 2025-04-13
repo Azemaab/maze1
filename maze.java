951df75e7894dbbb6f0632f5521c8caff1e6d6f7
Azemaab <azemaabdybekova@gmail.com>
2025-04-13 23:19:27 +0600

    Display path with '*' while printing maze

--- maze.java
+++ maze.java
@@ -87,4 +87,25 @@ class Pathfinder {
             }
         }
         return false;
+    }
+}
+
+// MazePrinter class
+class MazePrinter {
+    public static void printMaze(int[][][] maze, boolean[][] path) {
+        int height = maze.length;
+        int width = maze[0].length;
+
+        for (int y = 0; y < height; y++) {
+            // print top walls
+            for (int x = 0; x < width; x++) {
+                System.out.print("+");
+                System.out.print(maze[y][x][1] == 1 ? "   " : "---");
+            }
+            System.out.println("+");
+
+            // print side walls and path
+            for (int x = 0; x < width; x++) {
+                System.out.print(maze[y][x][0] == 1 ? " " : "|");
+                System.out.print(path != null && path[y][x] ? " * " : "   ");
         
